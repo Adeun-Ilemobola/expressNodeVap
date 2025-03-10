dd1e6da4b3644c5de648bc02e60730eccf694ced
“gave” <adeun2020@gmail.com>
2025-03-10 00:32:08 -0700

    Updated user routes and updated session route

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
-import express , { Request, Response, NextFunction , ErrorRequestHandler  } from 'express';
+import express, { Request, Response, NextFunction, ErrorRequestHandler } from 'express';
 import cors from 'cors';
-import {addDays, idGenerator} from "./Utility";
+import { addDays, idGenerator } from "./Utility";
 import connectToDatabase from "./Configure/db";
 import User from "./Configure/user";
-import {DateTime} from "luxon";
+import { DateTime } from "luxon";
 import CryptoJS from "crypto-js";
+import morgan from 'morgan';
 
 import Session from "./Configure/Session";
 import Note from "./Configure/Note";
 import Folder from "./Configure/Folder";
 import NotePath from './routes/NoteFile';
 import FolderPath from './routes/Folder';
+import UserRoute from './routes/User';
 
 type INote = {
     customId: string;
@@ -28,8 +30,11 @@ const app = express();
 const port = 3000;
 connectToDatabase()
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors())
+
+
+app.use(morgan('dev'));
 
 type session = {
     user: { username: string, email: string, id: string },
@@ -46,9 +51,9 @@ app.post('/api/Session', async (req, res) => {
     // console.log(" session validating endpoint")
 
     const body: session | null = req.body;
-    
+
     if (!body) {
-        res.status(404).send({error: "Invalid session", data: null});
+        res.status(404).send({ error: "Invalid session", data: null });
         return;
     }
     try {
@@ -68,7 +73,7 @@ app.post('/api/Session', async (req, res) => {
                 // console.log(" there is no user and there's a session ", item)
             }
             // console.log({error: "User not found", data: null})
-            res.status(404).send({error: "User not found", data: null});
+            res.status(404).send({ error: "User not found", data: null });
 
 
             return;
@@ -76,26 +81,36 @@ app.post('/api/Session', async (req, res) => {
 
         if (!getSession) {
             // console.log({error: "Session not found", data: null})
-            res.status(404).send({error: "Session not found", data: null});
+            res.status(404).send({ error: "Session not found", data: null });
             return;
         }
 
         const Date = DateTime.now();
         const sessionDate = DateTime.fromISO(getSession.expire)
-       
+
 
         if (Date.toMillis() >= sessionDate.toMillis()) {
-            res.status(400).send({error: "Invalid session expired", data: null});
+            res.status(400).send({ error: "Invalid session expired", data: null });
 
             return;
         }
-        res.status(200).send({error: null, data: body});
+        const newSession:session ={
+            user: {
+                username: ActiveUser.username,
+                email: ActiveUser.email,
+                id: ActiveUser.customId
+            },
+            id: getSession.customId,
+            expire: sessionDate.toFormat('yyyy-MM-dd HH:mm:ss')
+
+        }
+        res.status(200).send({ error: null, data: newSession });
 
 
     } catch (err) {
 
         console.log(err)
-        res.status(500).send({error: "Invalid session", data: null});
+        res.status(500).send({ error: "Invalid session", data: null });
 
     }
 
@@ -104,10 +119,10 @@ app.post('/api/Session', async (req, res) => {
 app.post('/api/Register', async (req, res) => {
     const body = req.body;
     if (!body) {
-        res.status(401).send({error: "Invalid body", data: null});
+        res.status(401).send({ error: "Invalid body", data: null });
         return;
     }
-   
+
     try {
         const user = await User.create({
             email: body.email,
@@ -116,14 +131,14 @@ app.post('/api/Register', async (req, res) => {
             name: body.name,
         })
         if (!user) {
-            res.status(400).send({error: "Invalid user the user end ", data: null});
+            res.status(400).send({ error: "Invalid user the user end ", data: null });
         }
-        res.status(201).json({message: "User registered successfully", data: user});
+        res.status(201).json({ message: "User registered successfully", data: user });
 
 
     } catch (err) {
         console.log(err)
-        res.status(500).send({error: "Unable to register", data: null});
+        res.status(500).send({ error: "Unable to register", data: null });
         return;
     }
 
@@ -134,24 +149,24 @@ app.post('/api/Login', async (req, res) => {
         const body: { username: string, password: string } | null = req.body;
         console.log(body);
         if (!body) {
-            res.status(401).send({error: "Invalid Credentials", data: null});
+            res.status(401).send({ error: "Invalid Credentials", data: null });
             return;
         }
-//     verify the user exist
+        //     verify the user exist
 
         const getUser = await User.findOne({
             username: body.username
         })
 
-//    compare the users password to the using found password hushed
+        //    compare the users password to the using found password hushed
         if (!getUser) {
-            res.status(401).send({error: "user not found", data: null});
+            res.status(401).send({ error: "user not found", data: null });
             return;
         }
 
         const inputPassword = CryptoJS.SHA256(body.password).toString();
         if (inputPassword != getUser.password) {
-            res.status(401).send({error: "Invalid password is not a much", data: null});
+            res.status(401).send({ error: "Invalid password is not a much", data: null });
             return;
         }
         await Session.deleteMany({
@@ -159,8 +174,8 @@ app.post('/api/Login', async (req, res) => {
         })
 
 
-//     the user is a much create a session to back to the user
-        const startDate = DateTime.utc().plus({days: 1});
+        //     the user is a much create a session to back to the user
+        const startDate = DateTime.utc().plus({ days: 1 });
         const newSession = await Session.create({
             user: {
                 username: getUser.username,
@@ -172,13 +187,13 @@ app.post('/api/Login', async (req, res) => {
         })
 
         if (!newSession) {
-            res.status(401).send({error: "Invalid Session can not be make ", data: null});
+            res.status(401).send({ error: "Invalid Session can not be make ", data: null });
             return;
         }
 
         res.status(201).send({
             error: null, data: {
-                user: {...newSession.user},
+                user: { ...newSession.user },
                 id: newSession.customId,
                 expire: newSession.expire
             }
@@ -187,98 +202,103 @@ app.post('/api/Login', async (req, res) => {
     } catch (err) {
         console.log(err)
 
-        res.status(500).send({error: "Unable to login", data: null});
+        res.status(500).send({ error: "Unable to login", data: null });
         return;
     }
 
 
 });
 
-app.post('/api/userCollection',async (req, res) => {
+app.post('/api/userCollection', async (req, res) => {
     try {
-        const {userID} = req.body as {userID:string};
+        const { userID } = req.body as { userID: string };
         if (!userID) {
-            res.status(401).send({error: "Invalid Credentials", data: null});
+            res.status(401).send({ error: "Invalid Credentials", data: null });
             return;
         }
 
         const note = await Note.find({
             userId: userID
-        },{
+        }, {
             customId: 1,
-            text:1,
-            title:1,
+            text: 1,
+            title: 1,
             folderID: 1,
             userId: 1,
-            _id:0
+            _id: 0
         })
 
-        const  folder = await Folder.find({
+        const folder = await Folder.find({
             userId: userID
 
-        },{
+        }, {
             customId: 1,
-            name:1,
+            name: 1,
             userID: 1,
         })
 
 
-        const NoteFormat = note.map(op=>{
+        const NoteFormat = note.map(op => {
             return {
-                id:op.customId,
-                userId:op.userId,
-                text:op.text,
-                folderID:op.folderID,
-                name:op.title,
+                id: op.customId,
+                userId: op.userId,
+                text: op.text,
+                folderID: op.folderID,
+                name: op.title,
             }
         })
 
 
-        const FolderFormat = folder.map(op=>{
+        const FolderFormat = folder.map(op => {
             return {
-                id:op.customId,
-                userId:op.userId,
-                name:op.name,
+                id: op.customId,
+                userId: op.userId,
+                name: op.name,
             }
         })
 
-        const struct = FolderFormat.map(item=>{
-            const kid = NoteFormat.filter(note=>note.folderID===item.id)
+        const struct = FolderFormat.map(item => {
+            const kid = NoteFormat.filter(note => note.folderID === item.id)
             return {
-                id:item.id,
-                name:item.name,
-                Children:[
-                    ...kid.map(child=>({id:child.id, name:child.name})),
+                id: item.id,
+                name: item.name,
+                Children: [
+                    ...kid.map(child => ({ id: child.id, name: child.name })),
                 ]
             }
         })
 
 
-        res.status(200).json({error: null, data: {
-                notesA:NoteFormat,
-                folderA:FolderFormat,
-                struct:struct
-            }})
+        res.status(200).json({
+            error: null, data: {
+                notesA: NoteFormat,
+                folderA: FolderFormat,
+                struct: struct
+            }
+        })
 
 
 
-    }catch(err){
+    } catch (err) {
         console.log(err)
-        res.status(500).send({error: "something went wrong", data: null});
+        res.status(500).send({ error: "something went wrong", data: null });
 
 
     }
 
 })
 
-app.use('/api/userFolder',FolderPath)
+
+app.use('/api/user', UserRoute)
+app.use('/api/userFolder', FolderPath)
 app.use('/api/userNote', NotePath)
 
-function logger(req: Request, res: Response, next: NextFunction) {
-    console.log(`[${new Date().toISOString()}] ${req.method} - ${req.url}`);
-    next();
-}
-app.use(logger);
+app.use((err: any, req: Request, res: Response, next: Function) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Something went wrong!' });
+});
+
+
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
--- src/routes/User.ts
+++ src/routes/User.ts
@@ -1,38 +1,129 @@
-import express , { Request, Response, NextFunction  , Router  } from 'express';
+import express, { Request, Response, NextFunction, Router } from 'express';
 import user from '../Configure/user';
+import CryptoJS from "crypto-js";
+import { log } from 'console';
 
 
 const UserRoute = Router();
-UserRoute.get('/:id' , async(req, res , next)=>{
-     const {id } = req.params
-     try{
-          if (!id ){
-               res.status(401).send({error: "Invalid Credentials", data: null})
+UserRoute.get('/:id', async (req, res, next) => {
+     const { id } = req.params
+     try {
+          if (!id) {
+               res.status(401).send({ error: "Invalid Credentials", data: null })
                return;
           }
-          const CurUser = await  user.findOne({
-               customId:id ,
-          },{
-               customId:1,
-               username:1,
-               _id:0
+          const CurUser = await user.findOne({
+               customId: id,
+          }, {
+               customId: 1,
+               username: 1,
+               _id: 0
 
           }
-     )
-          if (!CurUser){
-               res.status(401).send({error: "User not found ", data: null})
+          )
+          if (!CurUser) {
+               res.status(401).send({ error: "User not found ", data: null })
                return;
 
           }
 
-          res.status(200).send({error:null, data: CurUser})
+          res.status(200).send({ error: null, data: CurUser })
 
           return;
 
-     }catch(err){
+     } catch (err) {
           console.log(err);
-          res.status(500).send({error: "Unable to get the user with the id of " + id, data: null})
-          
+          res.status(500).send({ error: "Unable to get the user with the id of " + id, data: null })
+
 
      }
 })
+
+
+
+UserRoute.put('/:id', async (req, res, next) => {
+     const { id } = req.params
+     const body = req.body as {username:string , password:string , email:string , newPassword:string , oldPassword:string , passwordConfirm:string , isNewpassword:boolean}
+     console.log(body);
+     
+     try {
+          if (!id || !body) {
+               console.log("body is not defined, and the ID is also not defined");
+               
+               res.status(401).send({ error: "Invalid Credentials", data: null })
+               return;
+          }
+          const getUser = await user.findOne({
+               customId: id,
+          })
+          if (!getUser) {
+               console.log("User not found");
+               
+               res.status(401).send({ error: "User not found ", data: null })
+               return;
+
+          }
+          let haspassword = body.isNewpassword ? CryptoJS.SHA256(body.newPassword).toString() : getUser.password;
+
+          if (body.isNewpassword){
+               console.log("New password");
+               
+               if (body.oldPassword && CryptoJS.SHA256(body.oldPassword).toString() === getUser.password) {
+                    console.log("This is the same password");
+                    
+                    res.status(401).send({ error: "This is the same password", data: null })
+                    return;
+               }
+               if (body.passwordConfirm !== body.newPassword) {
+                    console.log("Password and confirm password does not match");
+                    res.status(401).send({ error: "Password and confirm password does not match", data: null })
+                    return;
+               }
+          }
+
+         
+          const CrrUser = await user.findOneAndUpdate({
+               customId: id,
+          }
+               , {
+                    username: body.username,
+                    password: haspassword,
+                    email: body.email,
+                    
+               },
+               {
+                    new: true,
+               
+               }
+
+          )
+          if (!CrrUser) {
+
+               console.log("Unable to update the user");
+               res.status(401).send({ error: "Unable to update the user", data: null })
+               return;
+
+          }
+
+
+          console.log("Updated");
+          
+          res.status(200).send({ error: null, data: CrrUser })
+
+          return;
+
+     } catch (err) {
+          console.log(err);
+          res.status(500).send({ error: "Unable to update the user with the id of " + id, data: null })
+          next(err);
+
+
+
+     }
+})
+
+
+
+
+
+export default UserRoute;
